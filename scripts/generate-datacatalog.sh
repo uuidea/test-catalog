3186284926d30ea5063d032954788bbf0bbc13da
joepvgenuchten <joep.van.genuchten@gmail.com>
2026-01-20 17:01:50 +0100

    removing modules first

--- scripts/generate-datacatalog.sh
+++ scripts/generate-datacatalog.sh
@@ -2,6 +2,11 @@
 # uv run linkml-validate \
 #     -s simple_data_catalog_model/src/simple_data_catalog_model/data-catalog.yaml \
 #     data-catalog/data-catalog.yaml 
+
+# Remove the modules directory if it exists
+if [ -d "modules" ]; then
+    rm -rf modules
+fi
     
 uv run linkml-convert \
     -s simple_data_catalog_model/src/simple_data_catalog_model/data-catalog.yaml \
@@ -9,6 +14,7 @@ uv run linkml-convert \
     -o data-catalog/data-catalog.ttl \
     --prefix-file data-catalog/prefix.yaml \
     data-catalog/data-catalog.yaml
+
 
 
 uv run python -m simple_data_catalog_generator.create_data_catalog # this needs to be checked
